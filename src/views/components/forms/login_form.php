6b0fe23ba909862da27e19df0405b890230e8c45
SkyWors <skyworslegrand@gmail.com>
2025-01-01 16:35:51 +0100

    💄 Update login and register style

---
+++ src/views/components/forms/login_form.php
@@ -0,0 +1,27 @@
+<?php
+	use App\Utils\Lang;
+?>
+
+<form class="login" method="POST">
+	<h1><?= Lang::translate("LOGIN_TITLE") ?></h1>
+
+	<input
+		type="text"
+		name="email"
+		value="<?= isset($_POST["email"]) ? $_POST["email"] : "" ?>"
+		placeholder="<?= Lang::translate(key: "MAIN_EMAIL") ?>"
+		require
+		autofocus
+	>
+	<input
+		type="password"
+		name="password"
+		value="<?= isset($_POST["password"]) ? $_POST["password"] : "" ?>"
+		placeholder="<?= Lang::translate(key: "MAIN_PASSWORD") ?>"
+		require
+	>
+
+	<button class="submit" type="submit"><?= Lang::translate(key: "LOGIN_SUBMIT") ?></button>
+
+	<a class="link" href="/register"><?= Lang::translate(key: "LOGIN_REGISTER") ?></a>
+</form>
