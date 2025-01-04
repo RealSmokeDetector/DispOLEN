d426eab7904f1c33b1e6ed9b4fa7e3f7aa450a3a
marine404 <marine.gonnord.pro@gmail.com>
2025-01-04 11:39:43 +0100

    🗃️update database

--- src/Models/Entities/Reservation.php
+++ src/Models/Entities/Reservation.php
@@ -3,21 +3,29 @@
 namespace App\Models\Entities;
 
 class Reservation {
-	private ?string $uid_teacher;
-	private ?string $uid_student;
-	private ?string $uid_disponibilities;
+	private string $teacherUid;
+	private string $studentUid;
+	private string $disponibilitiesUid;
+	private ?int $typeId;
+	private ?int $reasonId;
+	private ?int $stateId;
+	private ?string $comment;
 
-	public function __construct(string $uid_teacher, string $uid_student, string $uid_disponibilities) {
-		$this->uid_teacher = $uid_teacher;
-		$this->uid_student = $uid_student;
-		$this->uid_disponibilities = $uid_disponibilities;
+	public function __construct(string $teacherUid, string $studentUid, string $disponibilitiesUid, int $typeId = null, int $reasonId = null, int $stateId = null, string $comment = null) {
+		$this->teacherUid = $teacherUid;
+		$this->studentUid = $studentUid;
+		$this->disponibilitiesUid = $disponibilitiesUid;
+		$this->typeId = $typeId;
+		$this->reasonId = $reasonId;
+		$this->stateId = $stateId;
+		$this->comment = $comment;
 	}
 
-	public function __set($name, $value): void {
+	public function __set($name, $value) : void {
 		$this->$name = $value;
 	}
 
-	public function __get($var): mixed {
+	public function __get($var) : mixed {
 		return $this->$var;
 	}
 }
