11b26a487e7b147f01b7967354fded3ad2a91c06
rachelyazbek789 <131340740+rachelyazbek789@users.noreply.github.com>
2025-01-15 19:20:13 +0100

    ✨Add tutor name reservation details

--- src/Controllers/Reservations/Details/ReservationDetailsController.php
+++ src/Controllers/Reservations/Details/ReservationDetailsController.php
@@ -6,12 +6,16 @@ use App\Configs\Path;
 use App\Models\Entities\Reservation;
 use App\Models\Repositories\ReservationRepository;
 use App\Utils\System;
+use App\Utils\ApplicationData;
+use App\Models\Repositories\UserRepository;
 
 class ReservationDetailsController {
 	public function render() : void {
 		$reservation = new Reservation(uid: $_GET["reservation"]);
 		$reservationRepo = new ReservationRepository(reservation: $reservation);
 		$reservationData = $reservationRepo->getInformation();
+		$userInformation = UserRepository::getInformations(uid: $reservationData["uid_teacher"]);
+		$name = ApplicationData::nameFormat(name: $userInformation["name"],surname: $userInformation["surname"], complete: false, toChange: 1);
 
 		if (
 			!isset($_GET["reservation"])
