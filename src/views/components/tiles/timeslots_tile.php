68018ca96fbf401a8336a3cda861d749a69fc6b9
Anthony MEUNIER <anthony.meunier@lyon.ort.asso.fr>
2025-01-21 10:39:21 +0100

    🚧 timeslots small refactor + design

--- src/views/components/tiles/timeslots_tile.php
+++ src/views/components/tiles/timeslots_tile.php
@@ -56,29 +56,32 @@
 		return false;
 	}
 	// timeslots from 8 am t 5pm
-	$todayTimeslots = generateTodayTimeslots(8, 17, 60);
-	?>
+	$todayTimeslots = generateTodayTimeslots(startHour: 8, endHour: 19, intervalMinutes: 30);
 
-<div class="tile disponibility_timeslots_tile" id="disponibility_timeslots_tile" data-uid="<?= $_SESSION ["user"] ["uid"]?>">
-<h1><?= Lang::translate(key: "DISPONIBILITY_TIMESLOTS_TITLE") ?></h1>
-<p id="timesolt_date" ><?= htmlspecialchars($date) ?></p>
-<div id="timeslots_container">
-	<?php
-		$startHour = 8;
-		$endHour = 19;
+?>
 
-		for ($hour = $startHour; $hour < $endHour; $hour++) {
-			$timeSlot = sprintf("%02d:00 - %02d:00", $hour, $hour + 1);
-			$isReserved = isReserved($teacherDisponibilities, $hour);
-	?>
+<div class="tile disponibility_timeslots_tile" id="disponibility_timeslots_tile">
+	<h1><?= Lang::translate(key: "DISPONIBILITY_TIMESLOTS_TITLE") ?></h1>
+	<p><?= htmlspecialchars($date) ?></p>
+	<div class="line"></div>
+	<div id="timeslots_container" class="timeslots">
+		<?php
+			$startHour = 8;
+			$endHour = 19;
 
-			<div class="line"></div>
-			<div class="timeslot">
-				<h2><?= htmlspecialchars($timeSlot) ?></h2>
-				<p><?= ($isReserved)? Lang::translate(key: "RESERVED") : Lang::translate(key: "AVAILABLE"); ?></p>
-			</div>
-	<?php
-		}
-	?>
-</div>
+			for ($hour = $startHour; $hour <= $endHour; $hour++) {
+				$timeSlot = sprintf("%02d:00", $hour);
+				$isReserved = isReserved($teacherDisponibilities, $hour);
+		?>
+				<div class="time_container">
+					<div class="timeslot">
+						<p><?= htmlspecialchars($timeSlot) ?></p>
+						<!-- <p><?= ($isReserved)? Lang::translate(key: "RESERVED") : Lang::translate(key: "AVAILABLE"); ?></p> -->
+					</div>
+					<div class="line"></div>
+				</div>
+		<?php
+			}
+		?>
+	</div>
 </div>
