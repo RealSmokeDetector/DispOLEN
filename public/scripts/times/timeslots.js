ad7102520175650dbd4dda6875d0abcd6dd418d7
Anthony MEUNIER <anthony.meunier@lyon.ort.asso.fr>
2025-01-25 23:08:47 +0100

    � fix timeslot

--- public/scripts/times/timeslots.js
+++ public/scripts/times/timeslots.js
@@ -10,40 +10,40 @@ const uid = document.getElementById("disponibility_timeslots_tile").dataset.uid;
 calendarDays.forEach(day => {
 	day.addEventListener("click", async function(event) {
 		dateTimeslot.innerText = event.target.dataset.date;
-		let url = "/api/reservations";
+		let url = "/api/reservation";
 		let reservations = await callApi(url , "post", {
 			"uid": uid,
 			"date_start": event.target.dataset.date
 		} );
 
-		console.log("reservations", await reservations);
 		const timeslots = document.querySelectorAll(".availbility_reserved");
-		console.log("timeslots", timeslots);
 
 		timeslots.forEach((value) =>{
 			value.style.height = 0;
 		});
 		await reservations.forEach((value,index) => {
-			lastIndex = index;
 			if (timeslots[index]) {
 				updateTimeslot(new Date(value.date_start), new Date(value.date_end), timeslots[index]);
 			} else {
-				createTimeslot(value.date_start, value.date_end);
+				createTimeslot(new Date(value.date_start), new Date(value.date_end));
+
 			}
 		});
 	});
 });
 
 function createTimeslot(dateStart, dateEnd) {
-	divTimelot = document.createElement("div");
-	divTimelot.classlisit.add("availbility_reserved");
-	divTimelot.style.position = "fixed";
-	divTimelot.style.height = (((dateEnd - dateStart) / 60000) * heightDiv) / (11 * 60 );
-	divTimelot.style.transform = "translateY" + (((start - dateStart) / (60 * 1000)) * heightDiv) / (11 * 60 ) + "px";
+	const divTimeslot = document.createElement("div");
+	divTimeslot.classList.add("availbility_reserved");
+	divTimeslot.style.position = "fixed";
+	divTimeslot.style.width = "90px";
+	divTimeslot.style.height = (((dateEnd - dateStart) / 60000) * heightDiv) / (11 * 60 ) + "px";
+	divTimeslot.style.transform = "translateY(" + (((dateStart - new Date(dateStart).setHours(8,0,0)) / (60 * 1000)) * heightDiv) / (11 * 60 ) + "px)";
+	containerTimeslot.append(divTimeslot);
 }
 
 function updateTimeslot(dateStart, dateEnd, htmlElement) {
-	htmlElement.style.height = (((dateEnd - dateStart) / 60000) * heightDiv) / (11 * 60 );
+	htmlElement.style.height = (((dateEnd - dateStart) / 60000) * heightDiv) / (11 * 60) + "px";
 	htmlElement.style.transform = "translateY(" + (((dateStart - new Date(dateStart).setHours(8,0,0)) / (60 * 1000)) * heightDiv) / (11 * 60 ) + "px)";
 }
 
