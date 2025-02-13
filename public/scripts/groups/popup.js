53df7f01719b375dfdb640c8587a33b7737bef1f
marine404 <marine.gonnord.pro@gmail.com>
2025-02-13 09:37:00 +0100

    ✨ add group

---
+++ public/scripts/groups/popup.js
@@ -0,0 +1,6 @@
+const addButton = document.getElementById("add_button");
+const groupPopup = document.getElementById("group_popup");
+
+addButton.addEventListener("click", () => {
+	groupPopup.style.display = "flex";
+});
