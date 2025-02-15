bed54e9ab84df021330839e7b0e43c24b329de3c
marine404 <marine.gonnord.pro@gmail.com>
2025-02-15 20:31:22 +0100

    ✨ update index for admin

--- src/views/layouts/index/index.php
+++ src/views/layouts/index/index.php
@@ -1,10 +1,16 @@
 <?php
 	use App\Configs\Path;
+	use App\Configs\Role;
 	use App\Utils\Lang;
+	use App\Utils\Roles;
 ?>
 
 <div class="index_container">
-	<?php require Path::COMPONENTS . "/tiles/reservations_list_tile.php"; ?>
+	<?php
+		if (Roles::check(userRoles: $roles, allowRoles: [Role::TEACHER, Role::STUDENT])) {
+			require Path::COMPONENTS . "/tiles/reservations_list_tile.php";
+		}
+	?>
 	<?php require Path::COMPONENTS . "/actions/calendar.php"; ?>
 
 	<div class="tile timeslots_tile" id="timeslots_tile" data-uid="<?= $_SESSION["user"]["uid"] ?>">
