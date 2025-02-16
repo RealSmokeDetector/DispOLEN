cf0d426a58955d96052c249691dd907585ec25ce
Anthony MEUNIER <anthony.meunier@lyon.ort.asso.fr>
2025-02-16 20:34:37 +0100

    ✨ add filter input

--- public/scripts/users/search.js
+++ public/scripts/users/search.js
@@ -1,4 +1,5 @@
 const searchInput = document.getElementById("control_panel_search");
+const orderSelect = document.getElementById("control_panel_filter");
 const userTiles = document.querySelectorAll("#user_tile");
 
 if (isElementExist(searchInput)) {
@@ -12,6 +13,28 @@ if (isElementExist(searchInput)) {
 			if (name.includes(search)) {
 				tile.style.display = "block";
 			}
+		});
+	});
+}
+
+if (isElementExist(orderSelect)) {
+	orderSelect.addEventListener("change", (event) => {
+		let order = event.currentTarget.value;
+		let sortedUserTiles = Array.from(userTiles).sort((firstElement, secondElement) => {
+			let firstText = firstElement.children[0].children[0].textContent.toLowerCase();
+			let secondText = secondElement.children[0].children[0].textContent.toLowerCase();
+
+			if (order === "a-z") {
+				return firstText.localeCompare(secondText);
+			} else if (order === "z-a") {
+				return secondText.localeCompare(firstText);
+			}
+		});
+
+		const parent = userTiles[0].parentNode;
+		parent.innerHTML = "";
+		sortedUserTiles.forEach(tile => {
+			parent.appendChild(tile);
 		});
 	});
 }
